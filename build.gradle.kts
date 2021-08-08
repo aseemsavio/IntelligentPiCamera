9a85edb7baf9ba87f9e9a93007d9c37fb8da885a
aseemsavio <aseemsavio3@gmail.com>
2021-08-08 18:12:58 +0530

    create fat jar

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,3 +26,22 @@ tasks.test {
 tasks.withType<KotlinCompile>() {
     kotlinOptions.jvmTarget = "11"
 }
+
+
+val mainClass = "com.aseemsavio.intelligentpicamera.AppKt" // replace it!
+
+tasks {
+    register("fatJar", Jar::class.java) {
+        archiveClassifier.set("all")
+        duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+        manifest {
+            attributes("Main-Class" to mainClass)
+        }
+        from(configurations.runtimeClasspath.get()
+            .onEach { println("add from dependencies: ${it.name}") }
+            .map { if (it.isDirectory) it else zipTree(it) })
+        val sourcesMain = sourceSets.main.get()
+        sourcesMain.allSource.forEach { println("add from sources: ${it.name}") }
+        from(sourcesMain.output)
+    }
+}
